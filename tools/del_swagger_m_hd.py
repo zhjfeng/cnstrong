5406df7b8e609894c4ab807d144cfedfb4399826
zhjfeng <15850512966@163.com>
2019-04-01 11:11:27 +0800

    update

--- tools/del_swagger_m_hd.py
+++ tools/del_swagger_m_hd.py
@@ -1,33 +1,41 @@
 #coding:utf-8
 import json
+import requests
 '''
 删除swagger中pad和m的接口
 '''
-def del_swagger_m_hd():
-	with open('D:/git/cnstrong/swagger.json','r', encoding='UTF-8') as f:
-		#json转dict，取出paths下的各个接口的键	
-		temp = json.load(f)
-		json_paths = temp['paths']
-		#print(type(temp))
-		key_json = json_paths.keys()
-		#print(list(key_json))
-		#取出含有m和hd的键，并在字典中删除键值
-		del_list = []
-		for i in key_json:
-			#print(i)
-			if "/hd/" in i:
-				del_list.append(i) 
+def get_swagger():
+	url = "http://eduplan.leke.cn/v2/api-docs"
+	response = requests.request("GET", url)
+	swagger_dict = json.loads(response.text)
+	return swagger_dict
 
-			elif "/m/" in i:
-				del_list.append(i)
+def del_swagger_m_hd_py():
+	temp = get_swagger()
+	paths_new = {k:v for k,v in temp['paths'].items() if '/hd/' not in k and '/m/' not in k}
+	temp.update(paths=paths_new)
+	return temp
 
-		for j in del_list:
-			del json_paths[str(j)]
-		#print(json_paths)
-		#替换paths的值返回
-		temp.update(paths=json_paths)
-		#lastjson = json.dumps(temp, ensure_ascii=False)
-		return temp
+with open("E:/git/cnstrong/test.json","w", encoding='UTF-8') as ff:
+	json.dump(del_swagger_m_hd_py(),ff,ensure_ascii=False)
 
-with open("D:/git/cnstrong/test.json","w", encoding='UTF-8') as ff:
-     json.dump(del_swagger_m_hd(),ff,ensure_ascii=False)
+#java式
+def del_swagger_m_hd_java():
+	temp = get_swagger()
+	json_paths = temp['paths']
+	key_json = json_paths.keys()
+	#取出含有m和hd的键，并在字典中删除键值
+	del_list = []
+	for i in key_json:
+		if "/hd/" in i:
+			del_list.append(i) 
+
+		elif "/m/" in i:
+			del_list.append(i)
+
+	for j in del_list:
+		del json_paths[str(j)]
+	#替换paths的值返回
+	temp.update(paths=json_paths)
+	#lastjson = json.dumps(temp, ensure_ascii=False)
+	return temp
