076d6137d8af0745fdb6a94c6f9d0410706b5de2
zhjfeng <15850512966@163.com>
2019-04-09 17:03:01 +0800

    批量加入志愿

--- eduplan/addcache.py
+++ eduplan/addcache.py
@@ -13,10 +13,10 @@ response = requests.request("POST", url, data=payload, headers=headers)
 '''
 url = "https://eduplan.leke.cn/auth/student/forecast/profession/getOptimizationCollegeOrMajor.htm"
 
-payload = "{\"curPage\":1,\"objectId\":\"5ca2ce665db0dc1a54df28f9\",\"pageSize\":80,\"rankType\":1}"
+payload = "{\"curPage\":1,\"objectId\":\"XXXXXXXXXXXX\",\"pageSize\":80,\"rankType\":1}"
 headers = {
     'Content-Type': "application/json",
-    'Cookie': "ticket = VFZSWlBRPT07SlNVakp5VW5KU1VqOzE2MTY="
+    'Cookie': "ticket = XXXXXXXXXXX"
     }
 response = requests.request("POST", url, data=payload, headers=headers)
 college_dict = json.loads(response.text)
@@ -33,6 +33,6 @@ for i in range(0,79):
 	payload = "{\"collegeId\":"+ str(collegeId) + ",\"collegeName\":\"" + str(collegeName) + "\",\"id\":" + str(id) + ",\"operateType\":1,\"proAliasName\":\"" + str(proAliasName) + "\",\"professionId\":" + str(professionId) +",\"key\":\"eduplan.volunteer.\"}"	
 	headers = {
 	    'Content-Type': "application/json",
-	    'Cookie': "ticket = VFZSWlBRPT07SlNVakp5VW5KU1VqOzE2MTY="
+	    'Cookie': "ticket = XXXXXXXXXXXXXXXXXX"
 	    }
 	response = requests.request("POST", url1, data=payload.encode(), headers=headers)
