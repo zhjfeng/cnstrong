4e33652fcc22db05dfc344ddfe4f5fc16b3f307d
zhjfeng <15850512966@163.com>
2019-12-17 15:55:18 +0800

    基础框架

---
+++ ai_UI_test/testcase/test_login.py
@@ -0,0 +1,37 @@
+
+import unittest
+from page.login_page import LoginPage
+from selenium import webdriver
+
+
+class Caselogin(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.url = "https://cas.leke.cn/login?service="
+        self.loginName = "955112"
+        self.password = "a1234567"
+
+    # 用例执行体
+    def test_login_mail(self):
+        # 声明LoginPage类对象
+        login_page = LoginPage(self.driver, self.url, "乐课网 - 我们的快乐课堂")
+        # 调用打开页面组件
+        login_page.open()
+        # 切换到登录框Frame
+        #login_page.switch_frame('x-URS-iframe')
+        # 调用用户名输入组件
+        login_page.input_loginName(self.loginName)
+        # 调用密码输入组件
+        login_page.input_password(self.password)
+        # 调用点击登录按钮组件
+        login_page.click_login()
+
+    def tearDown(self):
+        pass
+        #self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
