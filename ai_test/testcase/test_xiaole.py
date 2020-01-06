c86e25b71c1a7f7099b7425b0b3adc9bf4d4c740
zhjfeng <15850512966@163.com>
2020-01-06 17:14:13 +0800

    增加智能问答用例

--- ai_test/testcase/test_xiaole.py
+++ ai_test/testcase/test_xiaole.py
@@ -1,5 +1,21 @@
 '''
 @Time    : 2019/12/26 17:26
 @Author  : fzj
-@Desc    :
+@Desc    : 小乐demo
 '''
+
+from poium import Page, PageElement
+import sys
+sys.path.append("E:\\git\\cnstrong\\ai_test")
+from page.web import Web
+from page.s_main import Mainpage
+
+class TestXaiole():
+    def setup(self):
+        self.xiaole_page = Web.s_start().login('955112', 'a1234567').hi_xiaole()
+
+    def test_xiaole(self):
+        self.xiaole_page.question()
+
+    def teardown(self):
+        pass
